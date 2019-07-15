88f00a7facb9a2cb095c4bc80efc3d67337ab075
Lupita Cruz <lupitacruz20@hsamkckinley.org>
2019-07-15 09:35:42 -0500

    Initial Commit

---
+++ loginPageWithsplash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loginPageWithsplash
+//
+//  Created by SER-YCL12 on 7/15/19.
+//  Copyright © 2019 Lupita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
